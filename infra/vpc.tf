8a5a7259221fc2a48f54d4bd17dee851973b0200
masafumi330 <mihusamawasami@gmail.com>
2025-10-29 06:34:31 +0900

    update terraform: add NAT for pull ECR from ECS task

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -79,6 +79,25 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-nat-eip"
+  })
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[var.azs[0]].id
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-nat"
+  })
+
+  depends_on = [aws_internet_gateway.this]
+}
+
 resource "aws_route_table" "private" {
   for_each = aws_subnet.private
   vpc_id   = aws_vpc.main.id
@@ -92,6 +111,13 @@ resource "aws_route_table_association" "private" {
   for_each       = aws_subnet.private
   subnet_id      = each.value.id
   route_table_id = aws_route_table.private[each.key].id
+}
+
+resource "aws_route" "private_outbound" {
+  for_each               = aws_route_table.private
+  route_table_id         = each.value.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
 }
 
 resource "aws_security_group" "alb" {
